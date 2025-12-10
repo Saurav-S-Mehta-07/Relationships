3aaa212228c2c4d0e68adac5cd84d86ae4624ae4
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-10 16:32:39 +0530

    one-to-many 2nd approach

--- Models/customer.js
+++ Models/customer.js
@@ -13,7 +13,45 @@ const orderSchema = new Schema({
   item : String,
   price : Number,  
 });
+
+const customerSchema = new Schema({
+      name : String,
+      orders : [
+        { type: Schema.Types.ObjectId, ref: 'order' }
+      ]
+});
+
 const Order = mongoose.model("order",orderSchema);
+
+const Customer = mongoose.model("customer",customerSchema);
+
+const addCustomers= async()=>{
+    let cust1 = new Customer({
+        name : "Mayank Verma",
+    });
+
+    let order1 = await Order.findOne({item : "Samosa"});
+    let order2 = await Order.findOne({item : "chips"});
+
+    cust1.orders.push(order1);
+    cust1.orders.push(order2);
+
+    let res = await cust1.save();
+    console.log(res);
+}
+// addCustomers();
+
+const findCustomer = async ()=>{
+
+    //in this orders : objectId of order
+    // let res = await  Customer.find();
+
+    //in this orders : full details of order
+    let res = await Customer.find().populate("orders"); //here orders feild name
+    console.log(res[0]); //orders: [objectId]
+}
+findCustomer();
+
 const addOrders = async()=>{
     let res = await Order.insertMany([
         {item:"Samosa", price: 30},
@@ -22,4 +60,4 @@ const addOrders = async()=>{
     ]);
     console.log(res);
 }
-addOrders();
+// addOrders();
