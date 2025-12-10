99e93809ef576ca35e38355da2da9fc7ba29fef6
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-10 16:01:08 +0530

    one to few

--- Models/user.js
+++ Models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { Schema } = mongoose;
 
 main().then(()=>{
     console.log("mongoDb connection successful");
@@ -8,3 +8,30 @@ main().then(()=>{
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/relationshipDB');
 }
+
+const userSchema = new Schema({
+      username : String,
+      addresses : [
+        {
+            _id:false,
+            location : String,
+            city : String,
+        },
+      ],
+});
+
+const User = mongoose.model("user",userSchema);
+
+const addUsers = async()=>{
+    let user1 = new User({
+        username: "Rahul Kumar",
+        addresses : [
+            {location:"221B baker street", city : "London"},
+        ]
+    });
+    user1.addresses.push({location:"49T street", city:"US"});
+   let result =  await user1.save();
+   console.log(result);
+}
+
+addUsers();
